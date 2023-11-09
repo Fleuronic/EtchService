eb1c739470722aee12061556b99de59a00e3df36
Jordan Kay <jordan@fleuronic.com>
2023-11-09 17:34:06 -0600

    Comment out database for now

--- Sources/EtchService/Areas/Service+TeamSpec.swift
+++ Sources/EtchService/Areas/Service+TeamSpec.swift
@@ -4,16 +4,16 @@ import struct Etch.Team
 
 extension Service: TeamSpec where
 	API: TeamSpec,
-	API.TeamDetailsResult == APIResult<API.TeamDetails>,
-	Database: TeamSpec,
-	Database.TeamDetailsResult == DatabaseResult<API.TeamDetails?> {
+	API.TeamDetailsResult == APIResult<API.TeamDetails>/*,
+	 Database: TeamSpec,
+	 Database.TeamDetailsResult == DatabaseResult<API.TeamDetails?>*/ {
 	public func fetchDetails(forTeamWith id: Team.ID) -> AsyncStream<API.TeamDetailsResult> {
 		.init { continuation in
 			Task {
-				let localDetails = await database.fetchDetails(forTeamWith: id).value.value
-				if let localDetails {
-					continuation.yield(.success(localDetails))
-				}
+				// let localDetails = await database.fetchDetails(forTeamWith: id).value.value
+				// if let localDetails {
+				// 	continuation.yield(.success(localDetails))
+				// }
 
 				let remoteDetails = await api.fetchDetails(forTeamWith: id).value
 				continuation.yield(remoteDetails)
--- Sources/EtchService/Service.swift
+++ Sources/EtchService/Service.swift
@@ -7,20 +7,20 @@ import protocol Catenoid.Database
 
 public struct Service<
 	API: Catenary.API,
-	Database: Catenoid.Database,
+	// Database: Catenoid.Database,
 	TeamDetails
-> where Database.Store == Store<ReadWrite> {
+> /*where Database.Store == Store<ReadWrite>*/ {
 	let api: API
-	let database: Database
+	// let database: Database
 	let teamDetailsType: TeamDetails.Type
 
 	public init(
 		api: API,
-		database: Database,
+		// database: Database,
 		teamDetailsType: TeamDetails.Type
 	) {
 		self.api = api
-		self.database = database
+		// self.database = database
 		self.teamDetailsType = teamDetailsType
 	}
 }
@@ -28,5 +28,5 @@ public struct Service<
 // MARK: -
 public extension Service {
 	typealias APIResult<Resource> = API.Result<Resource>
-	typealias DatabaseResult<Resource> = Database.Result<Resource>
+	// typealias DatabaseResult<Resource> = Database.Result<Resource>
 }
